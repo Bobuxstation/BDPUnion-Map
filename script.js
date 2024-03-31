c126bc14e8ad28b6a0a9b400ea5486e45b9f2be5
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2024-03-31 13:33:09 +0700

    Inital Commit

---
+++ script.js
@@ -0,0 +1,222 @@
+var countryData = {
+    "rafidland": {
+        "flag": 'rafidland.svg',
+        "President": "Rafid",
+        "Land Name": "Rafid land",
+        "Capital City": "Rafidus",
+        "Independence": "9 September 1999",
+        "Ideology": "liberalism",
+        "Currency": "Difar",
+        "States": "19",
+        "Main Language": "english",
+        "Resources": "diamond gold iron oil and wood",
+        "Population": "99.607.953",
+        "Troops": "991.960",
+        "Main Animal": "cat",
+        "Religion": "muslim (90%) hindu (10%)",
+        "news": {
+            "Rafid Land has relocated its cities underground, using the surface structures as decoys to detect potential nuclear threats, while ensuring the safety of all citizens residing in the subterranean cities.": "#",
+            "Rafid Land has commenced the construction of underground/undersea highways, linking it with the central continent via Zeanland.": "#",
+        }
+    },
+    "nandaland": {
+        "flag": "nandaland.svg",
+        "Land Name": "Nanda land",
+        "Capital City": "Nandus",
+        "Independence": "25 May 1863",
+        "Ideology": "freedom",
+        "Currency": "ndesh",
+        "States": "17",
+        "Main Language": "english",
+        "Resources": "oil",
+        "Population": "1 750 000 000",
+        "Troops": "10 550 000",
+        "news": {
+            "Nandaland has successfully invaded Silatland with Arkaland, dividing it into North and South Silatland.": "#"
+        }
+    },
+    "zeanland": {
+        "flag": "zeanland.webp",
+        "Land Name": "Zeanland",
+        "Type": "Republic",
+        "Ideology": "Liberalism",
+        "National Anthem": "Meine Ort",
+        "States": "15",
+        "Capital City": "Glorreiche",
+        "Economy": "BU dollar",
+        "Main Language": "German",
+        "Population": "917.729.843",
+        "Active Troops": "27.769.895",
+        "Bans": "Smoking, drugs, money laundry",
+        "Religion": "Islam (97.6%), Christian (2.63%), Buddha (1.52%)",
+        "Race Population": "Asian Mongoloid (64.8%), Malayan Mongoloid (36.2%), Caucasian (9.6%), Melanenoid (0.3%)",
+        "Zeanland Resources (Farm Crops)": "Barley, Rice, Wheat, Black Pepper",
+        "Stuffs Made in Zeanland": "Kasius laptops, LG Smart TVs, and more",
+        "news": {
+            "Zeanland bans cinema from disallowing people to bring outside food": "#",
+            "Zeanland bans limited account on steam": "#",
+            "Conventions or exhibitions in zeanland are no longer allowed to have a ticket to enter": "#",
+            "Zeanland re-bans crossplaying again": "#",
+            "Zeanland goverment of social media has banned the clown emoji due to cyber bullying": "#",
+            "In zeanland, all apple products that contain lightning port must be changed to usb c": "#",
+            "Zeanland has now banned any means of spicy because citizens have protested": "#",
+            "In zeanland, kigurumi mask studios are forced to deflate their prices": "#",
+            "Zeanland now bans nitro due to unfairness": "#",
+            "Zeanland has now launched the first cosplayer in space": "#",
+            "Crossword puzzles are banned due to citizen's hatery over crossword puzzles": "#",
+        }
+    },
+    "silatland": {
+        "flag": 'silatland.svg',
+        "Land Name": "Silatland",
+        "Capital City": "Sakti City",
+        "Leader": "King Silat IV",
+        "Independence": "4th april 2004",
+        "Ideology": "Communism",
+        "Currency": "Bakti",
+        "States": "1",
+        "Main Language": "english",
+        "Population": "50 000 000",
+        "Troops": "10 000 000",
+        "news": {
+            "Silat Land has inaugurated its first nuclear power plant, a development with potential implications. The concern arises over the possibility of nuclear weapon acquisition.": "#"
+        }
+    },
+    "asatland": {
+        "flag": 'asatland.webp',
+        "Land Name": "Asatland",
+        "President": "Asat",
+        "Independence": "6 September 1869",
+        "Ideology": "liberal democracy",
+        "Currency": "asatbucks or BU$",
+        "Resources/Exports": "oil, gold, copper, tin, livestock and coal",
+        "GDP": "2.3 Trillion BU$",
+        "States": "6 (Capital island, Narvian island, North Asatka, West mainland, East mainland, Asatland mandate of silatland)",
+        "Capital City": "Dahlia City (formerly known as arsyadnegara)",
+        "Megacities": "6 (Dahlia City, BigBurg, Arkarta, Classic town, Farmlands, Farmtopia, sutown)",
+        "Main Language": "English, Indonesian and Narvian",
+        "Population": "4.2 bil",
+        "Religion": "Islam (89%), Christianity (5%), Judaism (3%), Others (3%) (Mostly atheism)",
+        "Troops": "6.9M prepared, 20M emergency",
+        "news": {
+            "Asatland's new high-speed metro now seamlessly connects all islands to a central hub on the mainland, marking the completion of its construction.": "#",
+            "Asatland's government has transitioned from fascism to liberal democracy, heralding a new era of reform.": "#",
+            "The inauguration of Asatland's inaugural nuclear power plant on Narvian Island has resulted in 100% of homes now receiving reliable electricity, a significant increase from the previous 85% supplied by coal power plants.": "#",
+            "Narvian City's eastern region faced bombardment from the Welan Empire, resulting in an exchange of 650 casualties.": "#",
+            "Asatland has achieved a successful invasion of East Silatland, now establishing it as a puppet state under Asatland's control.": "#",
+            "Oil reserves discovered off the coast of Narvian Island prompt Welan to prepare for invasion.": "#",
+            "After days of riots, Asatland's longstanding president, Al-Fonso, has stepped down following over three decades of rule, signifying a new era in the nation's history.": "#",
+            "Silatland spies operating on Narvian Island have been apprehended and deported back to Silatland, with a total of 40 individuals discovered.": "#",
+            "Asatland's President, Al-Fonso, has initiated a shift in the country's ideology from liberal democracy to fascism, sparking widespread protests across the nation.": "#",
+            "Asatland has updated its flag, removing the symbol of the crown.": "#",
+        }
+    },
+    "farrasland": {
+        "flag": 'farrasland.svg',
+        "Land Name": "Farrasland",
+        "Capital City": "مدينة فرس",
+        "Independence": "20 june 1880",
+        "Ideology": "Islamic",
+        "Currency": "Dinar",
+        "States": "1",
+        "Main Language": "Arabic",
+        "Population": "290 000 000",
+        "Troops": "6 000 000",
+        "news": {
+            "Farrasland teeters on the brink of civil war as the majority of non-Muslims advocate for the creation of a separate nation.": "#",
+            "Farrasland has re-entered the BDP Union following diplomatic efforts to restore peace.": "#",
+            "Farrasland's economy has surged, surpassing that of Silatland and the Welan Empire, yet it remains beneath Rafidland's economic prowess.": "#",
+            "Farrasland has shifted its capital from \"Farrus\" to \"Madinat Faras\" upon the completion of the new capital's construction.": "#",
+            "Farrasland withdrew from the BDP Union following conflicts.": "#"
+        }
+    },
+    "arkaland": {
+        "flag": "arka_land_flag_2.png",
+        "President": "arka",
+        "Land Name": "Arkaland",
+        "Capital City": "arkus",
+        "Independence": "28 September 1716",
+        "Ideology": "democracy",
+        "Currency": "BU dollar",
+        "States": "20",
+        "Main Language": "english",
+        "Resources": "gold, oil, tungsten, copper, diamonds",
+        "Population": "8.2bil",
+        "Troops": "42.0m all active",
+        "Main Animal": "CAT NAMED DOG",
+        "news": {
+            "Shooting in Zean land embassy in Arkaland": "#",
+            "PH has been banned!": "https://docs.google.com/document/d/1sNHpn3yst4Dh-tonqrN6c4wzkvYJl35UeFDfmWgGfvE/edit?usp=sharing",
+            "Massive bombing in Arkaland capital.": "https://docs.google.com/document/d/1lERWSLShP6q_oqWYtbe5R8rD3dwc4pWscRs1WPLlpxQ/edit?usp=sharing",
+            "School shooting": "https://docs.google.com/document/d/16Om8GxuZz4W3iowSPiMZBRSja6dhthQs8Nlvuvottig/edit?usp=sharing",
+            "Cryptocurrency is legalized": "https://docs.google.com/document/d/1ee8S9umvprNcFOBGBQxX4HzxMBgacOC1p7Zt1yw2b2g/edit?usp=sharing"
+        }
+    },
+    "welan": {
+        "flag": 'welan.svg',
+        "Land Name": "The Great Welan Empire",
+        "Capital City": "Welan City",
+        "Independence": "1 october 2001",
+        "Ideology": "Fascism",
+        "Currency": "Silver",
+        "States": "5",
+        "Main Language": "Welanese (cyrillic)",
+        "Population": "89 000 000",
+        "Troops": "100 000",
+        "news": {
+            "Under Arkaland's rule as a puppet state, the Welan Empire has seen a notable rise in life expectancy.": "#",
+            "Welan empire has surenderred shortly after having its capital bombed by Arkaland forces": "#",
+            "Welan empire have obtained nuclear weapons, marking the start of a new war.": "#",
+        }
+    }
+}
+
+document.querySelectorAll("path").forEach(function (island) {
+    // Ensure the countryData for this island exists
+    if (countryData[island.className.baseVal]) {
+        island.onclick = function () {
+            document.getElementById("countrydesc").style.display = "block";
+            var fullstr = ``;
+            const country = countryData[island.className.baseVal];
+
+            if (country.flag === false) {
+                document.getElementById('flag').src = '';
+                document.getElementById('object').data = '';
+            } else if (country.flag.includes('svg')) {
+                document.getElementById('object').data = country.flag;
+                document.getElementById('flag').src = '';
+            } else {
+                document.getElementById('flag').src = country.flag;
+                document.getElementById('object').data = '';
+            }
+            document.getElementById('object').innerHTML = '';
+            document.getElementById("countryName").innerHTML = '<a onclick="this.parentNode.parentNode.style.display = `none`">< </a>' + country['Land Name'];
+
+            if (!country['news']) {
+                document.getElementById('newsholder').innerText = 'No news availible!'
+            } else {
+                document.getElementById('newsholder').innerText = ''
+            }
+
+            Object.keys(country).forEach(function (item, i) {
+                if (item == 'flag') return;
+                if (item == 'news') {
+                    Object.keys(country[item]).forEach(function (item, i) {
+                        let newscard = document.createElement('div')
+                        newscard.innerText = item
+                        newscard.onclick = function () {
+                            window.location.href = country['news'][item]
+                        }
+                        document.getElementById("newsholder").append(newscard)
+                    })
+                    return
+                }
+
+                let currline = `<b>${item}</b>: ${country[item]}<br>`;
+                fullstr += currline;
+            });
+
+            document.getElementById("desc").innerHTML = fullstr;
+        }
+    }
+});
