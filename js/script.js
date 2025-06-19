5df267222429f8942a53f2c56b2403a1a245de34
Bobuxstation <91587018+Bobuxstation@users.noreply.github.com>
2025-06-19 11:16:27 +0700

    rescaled flags

--- js/script.js
+++ js/script.js
@@ -20,33 +20,19 @@ async function setNations() {
             island.setAttribute("class", "hoverable")
 
             island.onclick = function () {
-                document.getElementById("countrydesc").style.display = "block";
                 var fullstr = ``;
-                const country = countryData[islandClass];
+                var country = countryData[islandClass];
 
+                document.getElementById("countrydesc").style.display = "block";
+                document.getElementById("countryName").innerHTML = '<a onclick="this.parentNode.parentNode.style.display = `none`" style="float: right;display: flex;justify-content: center;place-items: center;height: 32px;"><i class="fa-solid fa-xmark"></i></a>' + country['Country Name'];
+                
                 if (country.flag === false) {
-                    document.getElementById('flag').src = '';
-                    document.getElementById('flag').onclick = false
-
-                    if (document.getElementById('object')) { document.getElementById('object').remove() };
-                } else if (country.flag.includes('svg')) {
-                    if (document.getElementById('object')) { document.getElementById('object').remove() };
-
-                    let objectelem = document.createElement('object')
-                    objectelem.type = 'image/svg+xml'
-                    objectelem.data = country.flag
-                    objectelem.id = 'object'
-                    document.getElementById("imageContainer").prepend(objectelem)
-
                     document.getElementById('flag').src = '';
                     document.getElementById('flag').onclick = false
                 } else {
                     document.getElementById('flag').src = country.flag;
                     document.getElementById('flag').onclick = function () { window.open(country.flag) }
-
-                    if (document.getElementById('object')) { document.getElementById('object').remove() };
                 }
-                document.getElementById("countryName").innerHTML = '<a onclick="this.parentNode.parentNode.style.display = `none`" style="float: right;display: flex;justify-content: center;place-items: center;height: 32px;"><i class="fa-solid fa-xmark"></i></a>' + country['Country Name'];
 
                 if (!country['news']) {
                     document.getElementById('newsholder').innerText = 'No news availible!'
